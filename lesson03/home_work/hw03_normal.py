9a3e9dd84bf3cd449c1bb5763ec1cd8135f23fe9
Pandock <dgavlon@mail.ru>
2018-10-28 00:02:50 +0300

    normal 2/4

--- lesson03/home_work/hw03_normal.py
+++ lesson03/home_work/hw03_normal.py
@@ -3,7 +3,14 @@
 # Первыми элементами ряда считать цифры 1 1
 
 def fibonacci(n, m):
-    pass
+    fibo_list = [1, 1]
+    for i in range(m):
+        count = len(fibo_list) - 2
+        last = len(fibo_list) - 1
+        fibo_list.append(fibo_list[count] + fibo_list[last])
+    return fibo_list[n:m+1]
+
+print(fibonacci(1, 9))
 
 # Задача-2:
 # Напишите функцию, сортирующую принимаемый список по возрастанию.
@@ -12,7 +19,21 @@ def fibonacci(n, m):
 
 
 def sort_to_max(origin_list):
-    pass
+    start = origin_list[:]
+    res = []
+
+    while len(start) > 0:
+
+        lesser = start[0]
+
+        for i in range(len(start)):
+            if start[i] <= lesser:
+                lesser = start[i]
+        res.append(lesser)
+        start.remove(lesser)
+
+
+    return res
 
 sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
 
@@ -20,8 +41,14 @@ sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
 # Напишите собственную реализацию стандартной функции filter.
 # Разумеется, внутри нельзя использовать саму функцию filter.
 
+# Не ясно, какой именно фильтр надо было написать?
+# Т.е. я понял, как именно ф-ия фильтр работает, но что именно написать - не понял.
 
 # Задача-4:
 # Даны четыре точки А1(х1, у1), А2(x2 ,у2), А3(x3 , у3), А4(х4, у4).
 # Определить, будут ли они вершинами параллелограмма.
 
+# Задание не совсем понятно
+
+
+input()
