4bec16ba03b4bd889d15e99c71f00f0bc4488f94
Pandock <dgavlon@mail.ru>
2018-10-30 03:14:09 +0300

    hw_04 full

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -4,6 +4,8 @@
 # Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
 # Решить задачу двумя способами: с помощью re и без.
 
+import re
+
 line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'\
        'TUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqn'\
@@ -19,6 +21,31 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'qHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFa'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
+
+# Первый способ
+
+b = []
+
+step = ''
+
+for i in range(len(line)):
+    if line[i] == line[i].lower():
+        step = step + line[i]
+    else:
+        if step != '':
+            b.append(step)
+            step = ''
+
+b.append(step)
+
+print(b)
+
+# Второй способ
+
+a = re.split(r'[A-Z]', line)
+res = [i for i in a if i != '']
+
+print(res)
 
 
 # Задание-2:
@@ -45,9 +72,81 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+#Первый способ
+q = []
+step_two = ''
+
+for i in range(len(line_2)):
+    if line_2[i] == line_2[i].upper():
+        if step_two != '' and line_2[i+1:i+3] == line_2[i+1:i+3].upper():
+            step_two = step_two + line_2[i]
+        if line_2[i-2:i] == line_2[i-2:i].lower() and line_2[i+1:i+3] == line_2[i+1:i+3].upper():
+            step_two = step_two + line_2[i]
+    else:
+        if step_two != '':
+            q.append(step_two)
+            step_two = ''
+
+print(q)
+
+#Второй способ(неполный)
+s = re.findall(r'([A-Z]+(?=[A-Z]+[A-Z]))', line_2)
+#Не понимаю, как добавить проверку на 2 маленьких буквы перед большими.
+#Единственный вариант, который нашел - positive lookbehind - (?<=[a-z]+[a-z])
+#Выдает ошибку, не понимаю почему.
+
+print(s)
+
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+
+#Скрипт, создающий 2500-значное число в отдельном файле:
+
+from random import randint
+
+a = str()
+
+with open ('number.txt', 'w') as f:
+
+    for i in range(2500):
+        random_number = randint(0, 9)
+        f.write(str(random_number))
+
+#Вывод самой длинной последовательности:
+
+nums = list()
+with open('number.txt', 'r') as g:
+    nums = list(g.read())
+
+a = []
+stat = []
+
+
+for i in range(len(nums)):
+    if len(stat) == 0:
+        stat.append(nums[i])
+    elif nums[i] == nums[i - 1]:
+        stat.append(nums[i])
+    else:
+        a.append(stat)
+        stat = [nums[i]]
+a.append(stat)
+
+longest = a[0]
+
+for j in range(len(a)):
+    if a[j] > longest:
+        longest = a[j]
+
+result = ''
+for c in longest:
+    result = result + c
+
+print(result)
+
+
+input()
